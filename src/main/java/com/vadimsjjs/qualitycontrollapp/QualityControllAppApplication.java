25d945c337f34dead9539ebed9d735703d402c34
VadimSJJS <vadimvaluev72@gmail.com>
2026-07-14 16:16:54 +0300

    Initial commit

---
+++ src/main/java/com/vadimsjjs/qualitycontrollapp/QualityControllAppApplication.java
@@ -0,0 +1,13 @@
+package com.vadimsjjs.qualitycontrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityControllAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityControllAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vadimsjjs/qualitycontrollapp/QualityControllAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vadimsjjs.qualitycontrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityControllAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
